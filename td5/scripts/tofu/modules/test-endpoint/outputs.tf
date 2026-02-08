f802aeee408a5aa1df0d522a021a163b5ef355a7
TheoPhan77 <theo.phan@edu.esiee.fr>
2026-02-08 20:08:20 +0100

    Add infrastructure tests workflow

--- td5/scripts/tofu/modules/test-endpoint/outputs.tf
+++ td5/scripts/tofu/modules/test-endpoint/outputs.tf
@@ -1,3 +1,7 @@
-output "http_response" {
-  value = data.http.test_endpoint
+output "status_code" {
+  value = data.http.test_endpoint.status_code
+}
+
+output "response_body" {
+  value = data.http.test_endpoint.response_body
 }
